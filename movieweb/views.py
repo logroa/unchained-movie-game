73ee95bc4356cfc631f702ab2edd34f7ce417668
root <root@LAPTOP-4G719PAB.localdomain>
2021-07-06 16:31:58 -0500

    get first try wrong error patch

--- movieweb/views.py
+++ movieweb/views.py
@@ -101,9 +101,10 @@ def roleHandle(request, actor, movie):
 
 #end of game page
 def gameOver(request, game_id, entity, score, template_name = 'movieweb/gameover.html'):
-    t = Turn.objects.get(game_id = game_id, order = score)
-    t.last = True
-    t.save()
+    if score > 0:
+        t = Turn.objects.get(game_id = game_id, order = score)
+        t.last = True
+        t.save()
     return render(request, template_name, {"entity": entity, "score": score, "game_id": game_id})
 
 #validate actor at beginning of game
